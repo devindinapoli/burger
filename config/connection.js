926a8cd359b86eb49e5c70de2d2bc7d2fdd5fc2b
Devin DiNapoli <devindinapoli@gmail.com>
2018-04-05 17:43:47 -0400

    Jaws DB

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 var mysql = require("mysql");
 
 //connection information..
-var connection = mysql.createConnection({
+var connection; 
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+connection = mysql.createConnection({
     port: 3306,
     host: "localhost",
     user: "root",
     password: "",
     database: "burger_db"
-});
+  });
+}
 
 //establish our connection
 connection.connect(function(err) {
