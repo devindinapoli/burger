ff71aa2417f602d317e8fc1fd003b4d09f4cffc1
Devin DiNapoli <devindinapoli@gmail.com>
2018-04-05 14:20:01 -0400

    Added styles, not finished yet

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
